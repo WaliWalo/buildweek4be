6ba0c096751deb11e30155bb1c50958f833142c2
Ermal-code <72197798+Ermal-code@users.noreply.github.com>
2021-03-02 22:17:24 +0100

    almost fininshed endpoints

--- src/controllers/conversationController.js
+++ src/controllers/conversationController.js
@@ -1,17 +1,46 @@
 const ConversationModel = require("../models/ConversationModel");
 const MessageModel = require("../models/MessageModel");
 
-const getConversations = (req, res, next) => {
+const getConversations = async (req, res, next) => {
   try {
+    const conversation = await ConversationModel.find({
+      $or: [
+        { creator: req.params.userId },
+        {
+          creator2: req.params.userId,
+        },
+      ],
+    });
+    if (conversation) {
+      res.send(conversation);
+    } else {
+      const err = new Error();
+      err.message = `This conversation doesn't exist `;
+      err.httpStatusCode = 404;
+      next(err);
+    }
   } catch (error) {
     console.log(error);
+    next(err);
   }
 };
 
-const getMessages = (req, res, next) => {
+const getMessages = async (req, res, next) => {
   try {
+    const msgs = await MessageModel.find({
+      convoId: req.params.convoId,
+    });
+    if (msgs) {
+      res.send(msgs);
+    } else {
+      const err = new Error();
+      err.message = `This message doesn't exist `;
+      err.httpStatusCode = 404;
+      next(err);
+    }
   } catch (error) {
     console.log(error);
+    next(err);
   }
 };
 
